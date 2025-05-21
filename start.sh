670212a7d880969505cf0027d21317277999115d
arcasoftstudio <arcasoftstudio@gmail.com>
2025-05-21 23:06:52 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,6 +9,7 @@ apt update && apt install -y python3 python3-pip libgl1 libglib2.0-0
 echo "🐍 Installazione dipendenze Python..."
 pip3 install --upgrade pip
 pip3 install -r /workspace/cookie-guard-ai-server/requirements.txt
+pip3 install fastapi uvicorn pillow numpy ultralytics python-multipart
 
 echo "🚀 Avvio FastAPI..."
 cd /workspace/cookie-guard-ai-server
